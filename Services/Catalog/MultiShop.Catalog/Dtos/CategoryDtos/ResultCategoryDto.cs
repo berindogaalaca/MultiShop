1c298231961a077d52d8e1b351338811d4961bdc
Berin <dogaalacaaa@gmail.com>
2024-03-31 17:24:09 +0300

    complete dto

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+   
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
