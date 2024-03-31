c789bcbfaaf74ad7af2bcd15b149433159830774
Berin <dogaalacaaa@gmail.com>
2024-03-31 15:50:25 +0300

    complete appsetting

--- Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -1,8 +1,8 @@
 ﻿namespace MultiShop.Catalog.Entities
 {
-    public class ProductImages
+    public class ProductImage
     {
-        public string ProductImagesID { get; set; }
+        public string ProductImageID { get; set; }
         public string Image1 { get; set; }
         public string Image2 { get; set; }
         public string Image3 { get; set; }
